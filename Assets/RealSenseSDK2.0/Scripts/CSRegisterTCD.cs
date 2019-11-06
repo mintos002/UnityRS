b69544f09621e39f9931bec932356a8db9587f64
Alejandro Juan <aljuasin@i3b.upv.es>
2019-11-06 10:23:34 +0100

    working RsDevice

---
+++ Assets/RealSenseSDK2.0/Scripts/CSRegisterTCD.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace CSRegister
+{
+    public struct cRegImages
+    {
+        public IntPtr wThermal;
+        public IntPtr wThermalCM;
+        public IntPtr wThermalOverColor;
+    }
+
+    class CSRegisterTCD : IDisposable
+    {
+        #region PInvokes
+        [DllImport("lib_register_tcd_cpp.dll")]
+        static private extern IntPtr CreateRegisterClass();
+
+        [DllImport("lib_register_tcd_cpp.dll", CharSet = CharSet.Ansi)]
+        static private extern IntPtr CreateRegisterClassByFile(string file_path, bool invertRT);
+
+        [DllImport("lib_register_tcd_cpp.dll")]
+        static private extern void DisposeRegisterClass(IntPtr pCreateRegisterObject);
+
+        [DllImport("lib_register_tcd_cpp.dll")]
+        static private extern int call_getThermalWidth(IntPtr pCreateRegisterObject);
+
+        [DllImport("lib_register_tcd_cpp.dll")]
+        static private extern int call_getThermalHeight(IntPtr pCreateRegisterObject);
+
+        [DllImport("lib_register_tcd_cpp.dll")]
+        static private extern int call_getColorDepthWidth(IntPtr pCreateRegisterObject);
+
+        [DllImport("lib_register_tcd_cpp.dll")]
+        static private extern int call_getColorDepthHeight(IntPtr pCreateRegisterObject);
+
+        [DllImport("lib_register_tcd_cpp.dll", CharSet = CharSet.Ansi)]
+        static private extern IntPtr call_readRGBimage(IntPtr pCreateRegisterObject, string path);
+
+        [DllImport("lib_register_tcd_cpp.dll", CharSet = CharSet.Ansi)]
+        static private extern IntPtr call_readGRAYimage(IntPtr pCreateRegisterObject, string path);
+
+        [DllImport("lib_register_tcd_cpp.dll", CharSet = CharSet.Ansi)]
+        static private extern void call_writeRGBimage(IntPtr pCreateRegisterObject, IntPtr pimg, string filename, int width, int height);
+
+        [DllImport("lib_register_tcd_cpp.dll", CharSet = CharSet.Ansi)]
+        static private extern void call_writeRGBAimage(IntPtr pCreateRegisterObject, IntPtr pimg, string filename, int width, int height);
+
+        [DllImport("lib_register_tcd_cpp.dll", CharSet = CharSet.Ansi)]
+        static private extern void call_writeGRAYimage(IntPtr pCreateRegisterObject, IntPtr pimg, string filename, int width, int height);
+
+        [DllImport("lib_register_tcd_cpp.dll")]
+        static private extern void call_update(IntPtr pCreateRegisterObject, IntPtr t_img, IntPtr c_img, IntPtr d_img,
+            bool registerImag, float temp_min, float temp_max, float opacity, out int width, out int height/*,
+            ref IntPtr warpedThermal, ref IntPtr warpedThermalCM, ref IntPtr thermoOverColor*/);
+
+        [DllImport("lib_register_tcd_cpp.dll")]
+        static private extern IntPtr call_getWarpedThermal(IntPtr pCreateRegisterObject);
+
+        [DllImport("lib_register_tcd_cpp.dll")]
+        static private extern IntPtr call_getWarpedThermalCM(IntPtr pCreateRegisterObject);
+
+        [DllImport("lib_register_tcd_cpp.dll")]
+        static private extern IntPtr call_getThermoOverColor(IntPtr pCreateRegisterObject);
+
+        [DllImport("lib_register_tcd_cpp.dll", CharSet = CharSet.Ansi)]
+        static private extern void call_show3cImg(IntPtr pCreateRegisterObject, string name, IntPtr img, int width, int height, int waitKey);
+
+        [DllImport("lib_register_tcd_cpp.dll", CharSet = CharSet.Ansi)]
+        static private extern void call_removeShow(IntPtr pCreateRegisterObject, string name, bool all);
+        #endregion PInvokes
+
+        #region Members
+        private IntPtr m_pNativeObject; // Variable to hold c++ class
+        #endregion Members
+
+        public CSRegisterTCD()
+        {
+            this.m_pNativeObject = CreateRegisterClass();
+        }
+
+        public CSRegisterTCD(string file_path, bool invertRT)
+        {
+            this.m_pNativeObject = CreateRegisterClassByFile(file_path, invertRT);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        protected virtual void Dispose(bool bDisposing)
+        {
+            if (this.m_pNativeObject != IntPtr.Zero)
+            {
+                // Call the DLL Export to dispose this class
+                DisposeRegisterClass(this.m_pNativeObject);
+                this.m_pNativeObject = IntPtr.Zero;
+            }
+
+            if (bDisposing)
+            {
+                // No need to call the finalizer since we've now cleaned
+                // up the unmanaged memory
+                GC.SuppressFinalize(this);
+            }
+        }
+
+        // This finalizer is called when Garbage collection occurs, but only if
+        // the IDisposable.Dispose method wasn't already called.
+        ~CSRegisterTCD()
+        {
+            Dispose(false);
+        }
+
+        #region Wrapper methods
+        public int getThermalWidth()
+        {
+            return call_getThermalWidth(this.m_pNativeObject);
+        }
+
+        public int getThermalHeight()
+        {
+            return call_getThermalHeight(this.m_pNativeObject);
+        }
+
+        public int getColorDepthWidth()
+        {
+            return call_getColorDepthWidth(this.m_pNativeObject);
+        }
+
+        public int getColorDepthHeight()
+        {
+            return call_getColorDepthHeight(this.m_pNativeObject);
+        }
+
+        public IntPtr readRGBimage(string path)
+        {
+            return call_readRGBimage(this.m_pNativeObject, path);
+        }
+
+        public IntPtr readGRAYimage(string path)
+        {
+            return call_readGRAYimage(this.m_pNativeObject, path);
+        }
+
+        public void writeRGBimage(IntPtr pimg, string filename, int width, int height)
+        {
+            if (!(pimg == IntPtr.Zero))
+            {
+                call_writeRGBimage(this.m_pNativeObject, pimg, filename, width, height);
+            }
+            else
+            {
+                Console.WriteLine("ERROR.writeRGBimage: data is void.");
+            }
+        }
+
+        public void writeRGBAimage(IntPtr pimg, string filename, int width, int height)
+        {
+            if (!(pimg == IntPtr.Zero))
+            {
+                call_writeRGBAimage(this.m_pNativeObject, pimg, filename, width, height);
+            }
+            else
+            {
+                Console.WriteLine("ERROR.writeRGBAimage: data is void.");
+            }
+        }
+
+        public void writeGRAYimage(IntPtr pimg, string filename, int width, int height)
+        {
+            if (!(pimg == IntPtr.Zero))
+            {
+                call_writeGRAYimage(this.m_pNativeObject, pimg, filename, width, height);
+            }
+            else
+            {
+                Console.WriteLine("ERROR.writeGRAYimage: data is void.");
+            }
+        }
+
+        public void update(IntPtr t_img, IntPtr c_img, IntPtr d_img, bool registerImag,
+            float temp_min, float temp_max, float opacity, out int width, out int height/*,
+            ref IntPtr warpedThermal, ref IntPtr warpedThermalCM, ref IntPtr thermoOverColor*/)
+        {
+           call_update(this.m_pNativeObject, t_img, c_img, d_img, registerImag, temp_min, temp_max, opacity, out width, out height/*,
+               ref warpedThermal, ref warpedThermalCM, ref thermoOverColor*/);
+
+            //if(warpedThermal == IntPtr.Zero)
+            //{
+            //    Console.WriteLine("ERROR.update: warpedThermal is void.");
+            //}
+            //if (warpedThermalCM == IntPtr.Zero)
+            //{
+            //    Console.WriteLine("ERROR.update: warpedThermalCM is void.");
+            //}
+            //if (thermoOverColor == IntPtr.Zero)
+            //{
+            //    Console.WriteLine("ERROR.update: thermoOverColor is void.");
+            //}
+
+        }
+        
+        public IntPtr getWarpedThermal()
+        {
+            return call_getWarpedThermal(this.m_pNativeObject);
+        }
+        
+        public IntPtr getWarpedThermalCM()
+        {
+            return call_getWarpedThermalCM(this.m_pNativeObject);
+        }
+        
+        public IntPtr getThermoOverColor()
+        {
+            return call_getThermoOverColor(this.m_pNativeObject);
+        }
+
+        public void show3cImg(string name, IntPtr img, int width, int height, int waitKey)
+        {
+            call_show3cImg(this.m_pNativeObject, name, img, width, height, waitKey);
+        }
+
+        public void removeShow(string name, bool all)
+        {
+            call_removeShow(this.m_pNativeObject, name, all);
+        }
+
+        #endregion Wrapper methods
+
+    }
+}
---
+++ Assets/RealSenseSDK2.0/Scripts/RsDevice.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Threading;
+using UnityEngine;
+using Intel.RealSense;
+using System.Collections;
+using System.Linq;
+using System.Runtime.InteropServices;
+/// <summary>
+/// Manages streaming using a RealSense Device
+/// </summary>
+[HelpURL("https://github.com/IntelRealSense/librealsense/tree/master/wrappers/unity")]
+public class RsDevice : RsFrameProvider
+{
+    /// <summary>
+    /// The parallelism mode of the module
+    /// </summary>
+    public enum ProcessMode
+    {
+        Multithread,
+        UnityThread,
+    }
+
+    // public static RsDevice Instance { get; private set; }
+
+    /// <summary>
+    /// Threading mode of operation, Multithread or UnityThread
+    /// </summary>
+    [Tooltip("Threading mode of operation, Multithreads or Unitythread")]
+    public ProcessMode processMode;
+
+    // public bool Streaming { get; private set; }
+
+    /// <summary>
+    /// Notifies upon streaming start
+    /// </summary>
+    public override event Action<PipelineProfile> OnStart;
+
+    /// <summary>
+    /// Notifies when streaming has stopped
+    /// </summary>
+    public override event Action OnStop;
+
+    /// <summary>
+    /// Fired when a new frame is available
+    /// </summary>
+    public override event Action<Frame> OnNewSample;
+
+    /// <summary>
+    /// User configuration
+    /// </summary>
+    public RsConfiguration DeviceConfiguration = new RsConfiguration
+    {
+        mode = RsConfiguration.Mode.Live,
+        RequestedSerialNumber = string.Empty,
+        Profiles = new RsVideoStreamRequest[] {
+            new RsVideoStreamRequest {Stream = Stream.Depth, StreamIndex = -1, Width = 640, Height = 480, Format = Format.Z16 , Framerate = 30 },
+            new RsVideoStreamRequest {Stream = Stream.Infrared, StreamIndex = -1, Width = 640, Height = 480, Format = Format.Y8 , Framerate = 30 },
+            new RsVideoStreamRequest {Stream = Stream.Color, StreamIndex = -1, Width = 640, Height = 480, Format = Format.Rgb8 , Framerate = 30 }
+        }
+    };
+
+    private Thread worker;
+    private readonly AutoResetEvent stopEvent = new AutoResetEvent(false);
+    private Pipeline m_pipeline;
+
+
+
+    void OnEnable()
+    {
+        m_pipeline = new Pipeline();
+
+        using (var cfg = DeviceConfiguration.ToPipelineConfig())
+            ActiveProfile = m_pipeline.Start(cfg);
+
+        DeviceConfiguration.Profiles = ActiveProfile.Streams.Select(RsVideoStreamRequest.FromProfile).ToArray();
+
+        if (processMode == ProcessMode.Multithread)
+        {
+            stopEvent.Reset();
+            worker = new Thread(WaitForFrames);
+            worker.IsBackground = true;
+            worker.Start();
+        }
+
+        StartCoroutine(WaitAndStart());
+    }
+
+    IEnumerator WaitAndStart()
+    {
+        yield return new WaitForEndOfFrame();
+        Streaming = true;
+        if (OnStart != null)
+            OnStart(ActiveProfile);
+    }
+
+    void OnDisable()
+    {
+        OnNewSample = null;
+        // OnNewSampleSet = null;
+
+        if (worker != null)
+        {
+            stopEvent.Set();
+            worker.Join();
+        }
+
+        if (Streaming && OnStop != null)
+            OnStop();
+
+        if (ActiveProfile != null)
+        {
+            ActiveProfile.Dispose();
+            ActiveProfile = null;
+        }
+
+        if (m_pipeline != null)
+        {
+            // if (Streaming)
+            // m_pipeline.Stop();
+            m_pipeline.Dispose();
+            m_pipeline = null;
+        }
+
+        Streaming = false;
+    }
+
+    void OnDestroy()
+    {
+        // OnStart = null;
+        OnStop = null;
+
+        if (ActiveProfile != null)
+        {
+            ActiveProfile.Dispose();
+            ActiveProfile = null;
+        }
+
+        if (m_pipeline != null)
+        {
+            m_pipeline.Dispose();
+            m_pipeline = null;
+        }
+    }
+
+    private void RaiseSampleEvent(Frame frame)
+    {
+        var onNewSample = OnNewSample;
+        if (onNewSample != null)
+        {
+            onNewSample(frame);
+        }
+    }
+
+    /// <summary>
+    /// Worker Thread for multithreaded operations
+    /// </summary>
+    Align align = new Align(Stream.Color);
+    CSRegister.CSRegisterTCD regClass = new CSRegister.CSRegisterTCD(
+                    "C:\\Users\\aljuasin\\Desktop\\captura_tcd\\_imagenes\\patron\\output_imagesx640_80\\spaceing24_extrinsics_640_80.xml", true);
+    FrameSet fs;
+    int counter = 0;
+    FrameQueue qColor;
+    FrameQueue qDepth;
+    //Frame test;
+    //Predicate<Frame> matcher = new Predicate<Frame>();
+    private void WaitForFrames()
+    {
+        while (!stopEvent.WaitOne(0))
+        {
+            using (var frames = m_pipeline.WaitForFrames())
+            {
+                using (Frame aligned_frames = align.Process(frames))
+                {
+                    if (aligned_frames.IsComposite)
+                    {
+                        using (FrameSet fs = aligned_frames.As<FrameSet>())
+                        {
+                            using (Frame color = fs.ColorFrame)
+                            {
+                                using (Frame depth = fs.DepthFrame)
+                                {
+                                    RaiseSampleEvent(aligned_frames);
+                                    regClass.writeGRAYimage(depth.Data, "imageTest/depth_" + (counter) + ".png", 640, 480);
+                                    regClass.writeRGBimage(color.Data, "imageTest/color_" + (counter++) + ".png", 640, 480);
+
+                                }
+                            }
+                        }
+                    }
+                }
+
+                //Frame aligned_frames = align.Process(frames);
+                //test = aligned_frames.Clone();
+                //regClass.writeRGBimage();
+                //if (test.IsComposite)
+                //{
+                //    fs = test.As<FrameSet>(); 
+                //    regClass.writeRGBimage(fs.ColorFrame.Data, "imageTest/col_" + (counter) + ".png", 640, 480);
+                //    regClass.writeGRAYimage(fs.DepthFrame.Data, "imageTest/dep_" + (counter++) + ".png", 640, 480);
+                //}
+                //IntPtr a = frames.ColorFrame.Data;
+                //regClass.writeRGBimage(frames.ColorFrame.Data, "imageTest/col_" + (counter) + ".png", 640, 480);
+                //regClass.writeGRAYimage(aligned_frames.Data, "imageTest/dep_" + (counter++) + ".png", 640, 480);
+                //fs.Dispose();
+                //RaiseSampleEvent(frames);
+                //RaiseSampleEvent(aligned_frames);
+                //aligned_frames.Dispose();
+                //Marshal.FreeHGlobal(a);
+            }
+        }
+    }
+
+    void Update()
+    {
+        if (!Streaming)
+            return;
+
+        if (processMode != ProcessMode.UnityThread)
+            return;
+
+        FrameSet frames;
+        if (m_pipeline.PollForFrames(out frames))
+        {
+            using (frames)
+                RaiseSampleEvent(frames);
+        }
+    }
+
+}
